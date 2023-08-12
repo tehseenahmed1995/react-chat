fdc7cc290363dd02539da97483b19fa707aa6bf0
tehseenahmed <tahmad@taxbackinternational.com>
2023-08-12 21:27:01 +0500

    test commrt

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ window.Echo = new Echo({
   key: 'pusherkey',
   wsPort: 6001,
   wssPort: 6001,
-  wssHost: 'staging.tasteeapp.com',
+  wssHost: '127.0.0.1',
   cluster: 'mt1',
   forceTLS: false,
   authEndpoint: "http://staging.tasteeapp.com/broadcasting/auth",
