5f26e88d9f35d6086d8427e4c63700a50faa48ca
tehseenahmed <tahmad@taxbackinternational.com>
2023-08-12 21:07:11 +0500

    intial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+window.Pusher = Pusher;
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: 'pusherkey',
+  wsPort: 6001,
+  wssPort: 6001,
+  wssHost: 'staging.tasteeapp.com12',
+  cluster: 'mt1',
+  forceTLS: false,
+  authEndpoint: "http://staging.tasteeapp.com/broadcasting/auth",
+  // enabledTransports: ['ws', 'wss'], 
+  auth:
+    {
+        headers:
+        {
+            'Authorization': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+            'Accept' : 'application/json' ,       
+        }
+    }
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +34,28 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// window.Echo.join(`presence-broadcast-message`)
+//     .listen('.getChatMessage', (e) => {
+//         console.log(e);
+//     });
+const userId = 1;
+    window.Echo.join(`broadcast-restaurant-message.${userId}`)
+    .here((users) => {
+      console.log(users);
+    })
+    .joining((user) => {
+        console.log(user);
+    })
+    .leaving((user) => {
+        console.log(user);
+    })
+    .listen('.getChatMessage', (e) => {
+      console.log(e);
+    })
+    .error((error) => {
+        console.log(error);
+    });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
